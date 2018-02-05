2d5e26d1b40bb93e7c0264f375ed5501bff9d671
Predrag Falcic <falcicp@gmail.com>
2018-02-05 09:23:45 +0100

    Izgenerisane java klase od xsd fajlova i kreiran kontroler za povezivanje sa bazom i upisavanjem u bazu

---
+++ ObjavaNaucnihRadova/src/main/java/com/objava/naucnih/radova/model/cover_letter/package-info.java
@@ -0,0 +1,9 @@
+//
+// Diese Datei wurde mit der JavaTM Architecture for XML Binding(JAXB) Reference Implementation, v2.2.8-b130911.1802 generiert 
+// Siehe <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
+// Änderungen an dieser Datei gehen bei einer Neukompilierung des Quellschemas verloren. 
+// Generiert: 2018.02.05 um 09:13:44 AM CET 
+//
+
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.ftn.uns.ac.rs/naucni_radovi/cover_letter", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package com.objava.naucnih.radova.model.cover_letter;
