d3ea6862a9e0610bf6d7444ba51b5467cdc324dd
Predrag Falcic <falcicp@gmail.com>
2018-02-08 09:49:26 +0100

    Obrisan projekat

--- ObjavaNaucnihRadova/src/main/java/com/objava/naucnih/radova/model/naucni_rad/package-info.java
+++
@@ -1,9 +0,0 @@
-//
-// Diese Datei wurde mit der JavaTM Architecture for XML Binding(JAXB) Reference Implementation, v2.2.8-b130911.1802 generiert 
-// Siehe <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
-// Änderungen an dieser Datei gehen bei einer Neukompilierung des Quellschemas verloren. 
-// Generiert: 2018.02.05 um 09:12:30 AM CET 
-//
-
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.ftn.uns.ac.rs/naucni_radovi/naslovna", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.objava.naucnih.radova.model.naucni_rad;
