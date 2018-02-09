2ebfd483255bb42485824e931b447d5a9e590a95
Djapeghn <pkaljevic01@gmail.com>
2018-02-09 17:03:42 +0100

    Debagovanje dodavanja naucnog rada.

--- src/main/resources/static/app/controllers/autorHome.js
+++ src/main/resources/static/app/controllers/autorHome.js
@@ -6,20 +6,60 @@ angular.module('app')
 	
 	$scope.korisnik = AuthService.user;
 	
-	$scope.coverPage = {'author': []};
+	$scope.orcid2;
+	
+	$scope.coverPage = {};
+	$scope.coverPage.title;
+	$scope.autor = {};
+	$scope.autor.firstName;
+	$scope.autor.lastName;
+	$scope.autor.affiliation;
+	$scope.autor.email;
+	$scope.autor.phoneNumber;
+	//$scope.autor.orcid;
+	$scope.autorAdresa = {};
+	$scope.autorAdresa.street;
+	$scope.autorAdresa.streetNumber;
+	$scope.autorAdresa.city;
+	$scope.autorAdresa.country;
+	$scope.nrDef = {};
+	$scope.nrDef.abstractText;
+	$scope.keywords;
+	$scope.paper = {};
+	$scope.PrimaryHeader = {};
+	$scope.PrimaryHeader.primaryHeaderTitle;
+	$scope.PrimaryHeader.primaryHeaderText;
+	
+	
+	/*$scope.coverPage = {'author': []};
 	$scope.autor = {};
 	$scope.autorAdresa = {};
 	$scope.nrDef = {'coverPage': '', 'keywords': '', 'paper': ''};
 	$scope.keywords = [];
 	$scope.PrimaryHeader = {};
-	$scope.paper = {};
+	$scope.paper = {};*/
 	
 	$scope.addNaucniRad = function(){
 		
-		$scope.autor.authorAddress = $scope.autorAdresa;
+		/*$scope.autor.authorAddress = $scope.autorAdresa;
 		$scope.coverPage.author = $scope.autor;
 		$scope.nrDef.coverPage = $scope.coverPage;
 		$scope.nrDef.keywords = $scope.keywords;
+		$scope.paper.primaryHeader = $scope.PrimaryHeader;
+		$scope.nrDef.paper = $scope.paper;*/
+		$scope.autor.orcid = [$scope.orcid2];
+		$scope.keywords2 = $scope.keywords.split(',');
+		$scope.nrDef.keywords = [];
+		var listaKeywords = [];
+		for(var i=0; i<$scope.keywords2.length; i++) {
+			listaKeywords[i] = $scope.keywords2[i];
+		}
+		$scope.nrDef.keywords = listaKeywords;
+		
+		$scope.autor.authorAddress = $scope.autorAdresa;
+		$scope.coverPage.author = [$scope.autor];
+		$scope.nrDef.coverPage = $scope.coverPage;
+		//$scope.nrDef.keywords = $scope.keywords2;
 		$scope.paper.primaryHeader = $scope.PrimaryHeader;
 		$scope.nrDef.paper = $scope.paper;
 		
